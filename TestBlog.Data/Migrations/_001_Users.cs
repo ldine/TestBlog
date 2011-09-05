61b3e9d9eb3a978babbab8be6513841087edbcb1
Lawrence Dine <lawrencedine@googlemail.com>
2011-09-06 00:44:02 +0100

    Implemented FluentMigrator and used first migration

---
+++ TestBlog.Data/Migrations/_001_Users.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentMigrator;
+using System.Data;
+
+namespace TestBlog.Data.Migrations
+{
+    [Migration(1)]
+    public class _001_Users : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Users")
+                .WithColumn("User_Id").AsInt32().NotNullable().PrimaryKey()
+                .WithColumn("Username").AsString(100)
+                .WithColumn("Password").AsString(100);
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Users");
+        }
+    }
+}
